6d1d3f275682dd84537974813fee7dd6dcb19e16
Samuel Pereira <samuelsantana778@gmail.com>
2025-03-20 09:11:00 -0300

    logger

--- Learning/third/control.cpp
+++ Learning/third/control.cpp
@@ -1,35 +1,41 @@
+#include "logger.h"
+#include <thread>
+
+void example1() 
+{
+    int a = 5;
+    const char* name = "John";
+
+    Logger::set_priority(TracePriority);
+
+    Logger::Trace("Log test %d %s", a, name);
+    Logger::Debug("Log test %d %s", a, name);
+    Logger::Info("Log test %d %s", a, name);
+    Logger::Warn("Log test %d %s", a, name);
+    Logger::Critical("Log test %d %s", a, name);
+    Logger::Error("Log test %d %s", a, name);
+}
+
+void log_test(int n) 
+{
+    Logger::Info("%d", n);
+}
 
 
+void example2() 
+{
+    std::thread threads[10];
 
+    for(int i = 0; i < 10; i++) {
+        threads[i] = std::thread(log_test, i);
+    }
 
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-int main () {
-
-    
+int main () 
+{
+    example1();
+    example2();
+ 
     return 0;
 }
---
+++ Learning/third/logger.h
@@ -0,0 +1,88 @@
+#pragma once
+
+enum LogPriority 
+{
+    TracePriority, DebugPriority, InfoPriority, WarnPriority, ErrorPriority, CriticalPriority
+};
+
+class Logger
+{
+    
+private:
+    static LogPriority priority;
+
+public:
+    static void set_priority(LogPriority new_priority)
+    {
+        priority = new_priority;
+    }
+
+    template<typename... Args>
+    static void Trace(const char* message, Args... args) 
+    {
+        if(priority <= TracePriority)
+        {
+            printf("[Trace]\t");
+            printf(message, args);
+            print("\n");
+        }
+    }
+
+    template<typename... Args>
+    static void Debug(const char* message, Args... args) 
+    {
+        if(priority <= DebugPriority)
+        {
+            printf("[DEBUG]\t");
+            printf(message, args);
+            print("\n");
+        }
+    }
+
+    template<typename... Args>
+    static void Info(const char* message, Args... args) 
+    {
+        if(priority <= InfoPriority)
+        {
+            printf("[Info]\t");
+            printf(message, args);
+            print("\n");
+        }
+    }
+
+    template<typename... Args>
+    static void Error(const char* message, Args... args) 
+    {
+        if(priority <= TracePriority)
+        {
+            printf("[Error]\t");
+            printf(message, args);
+            print("\n");
+        }
+    }
+
+    template<typename... Args>
+    static void Warn(const char* message, Args... args) 
+    {
+        if(priority <= WarnPriority)
+        {
+            printf("[Warn]\t");
+            printf(message, args);
+            print("\n");
+        }
+    }
+
+    template<typename... Args>
+    static void Critical(const char* message, Args... args) 
+    {
+        if(priority <= CriticalPriority)
+        {
+            printf("[Critical]\t");
+            printf(message, args);
+            print("\n");
+        }
+    }
+
+};
+
+LogPriority Logger::priority = InfoPriority;
